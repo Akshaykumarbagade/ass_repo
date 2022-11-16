7450bec08a4e8842b7bb950b4f579eeb8b786e68
“pandya” <akshaykumarbagade18@gmail.com>
2022-11-16 15:05:38 +0000

    data version1.1

---
+++ hudugaru.sh
@@ -0,0 +1,1 @@
+echo "Prabhu, Chandru and Siddesh aspire to have a government job and try to lead a simple lifestyle. But their lives change dramatically when they attempt to unite two lovers".
---
+++ ranavickram.sh
@@ -0,0 +1,1 @@
+echo "Officer Vikrama sets out to investigate the case of a missing journalist. However, to get to the bottom of the truth, he must fight powerful gangsters."
